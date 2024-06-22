7fb08295ebdc6d892a0e9c1c5e7216cfd853034a
Alejandro <alexnachovc12@gmail.com>
2024-06-21 23:11:15 -0600

    Creating the Memory

--- main_6502.cpp
+++ main_6502.cpp
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+using Byte = unsigned char;
+using Word = unsigned short;
 
+using u32 = unsigned int;
+struct Mem {
+    
+    static constexpr u32 MAX_MEM = 1024 * 64;
+    Byte Data[MAX_MEM];
+
+    void Initialise() {
+        for (u32 i = 0; i < MAX_MEM; i++) {
+            Data[i] = 0;
+        }
+    };
+};
 struct CPU {
-    using Byte = unsigned char;
-    using Word = unsigned short;
 
     Word PC;            // Program Counter
     Word SP;            // Stack Pointer
@@ -20,16 +32,18 @@ struct CPU {
     Byte V : 1;         // Status Flag  
     Byte N : 1;         // Status Flag  
 
-    void Reset() {
+    void Reset( Mem& memory) {
         PC = 0xFFFC;
         SP = 0x0100;
         C = Z = I = D = B = V = N = 0;
         A = X = Y = 0;
+        memory.Initialise();
     }
 };
 
 int main() {
+    Mem mem;
     CPU cpu;
-    cpu.Reset();
+    cpu.Reset( mem );
     return 0;
 }
