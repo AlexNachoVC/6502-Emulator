f48a7ef144add37aff5bb8b6cc1718314ee09fcc
Alejandro <alexnachovc12@gmail.com>
2024-06-23 18:03:04 -0600

    Change project structure

---
+++ 6502Test/src/main_6502.cpp
@@ -0,0 +1,16 @@
+#include "main_6502.h"
+
+int main() {
+    Mem mem;
+    CPU cpu;
+    cpu.Reset( mem );
+    // Start - Inline a little program
+    mem[0xFFFC] = CPU::INS_JSR;
+    mem[0xFFFD] = 0x42;
+    mem[0xFFDE] = 0x42;
+    mem[0x4242] = CPU::INS_LDA_IM;
+    mem[0x4243] = 0x84;
+    // End - inline a little program
+    cpu.Execute( 9, mem);
+    return 0;
+}
