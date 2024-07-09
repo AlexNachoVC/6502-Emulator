a3762acdf972180fc742a61f7085a952b09c36cc
Alejandro <alexnachovc12@gmail.com>
2024-07-09 00:29:31 -0600

    PHP Implementation

--- 6502Lib/src/private/m6502.cpp
+++ 6502Lib/src/private/m6502.cpp
@@ -216,6 +216,10 @@ m6502::s32 m6502::CPU::Execute ( s32 Cycles, Mem& memory ) {
             {
                 PushByteOntoStack( Cycles, A, memory );
             } break;
+            case INS_PHP:
+            {
+                PushByteOntoStack( Cycles, PS, memory );
+            } break;
             default:
             {
                 printf("Instruction %d not handled\n", Ins);
