a315a1134764002ba20d834b2956516963b94b40
Alejandro <alexnachovc12@gmail.com>
2024-07-11 23:15:55 -0600

    Creating new test cpp, for And, Eor, Ora

---
+++ 6502Test/src/6502AND_EOR_ORA_Tests.cpp
@@ -0,0 +1,376 @@
+#include <gtest/gtest.h>
+#include "m6502.h"
+
+using namespace m6502;
+
+
+class M6502AndEorOraTests : public testing::Test {
+protected:
+    
+    Mem mem;
+    CPU cpu;
+
+    virtual void SetUp(){
+        cpu.Reset( mem );
+    }
+
+    virtual void TearDown(){
+    }
+
+    void TestLoadRegisterImmediate( Byte Opcode, Byte CPU::*Register );
+    void TestLoadRegisterZeroPage( Byte OpcodeToTest, Byte CPU::*RegisterToTest );
+    void TestLoadRegisterZeroPageX( Byte OpcodeToTest, Byte CPU::*RegisterToTest );
+    void TestLoadRegisterZeroPageY( Byte OpcodeToTest, Byte CPU::*RegisterToTest );
+    void TestLoadRegisterAbsolute( Byte OpcodeToTest, Byte CPU::*RegisterToTest );
+    void TestLoadRegisterAbsoluteX( Byte OpcodeToTest, Byte CPU::*RegisterToTest );
+    void TestLoadRegisterAbsoluteY( Byte OpcodeToTest, Byte CPU::*RegisterToTest );
+    void TestLoadRegisterAbsoluteXWhenCrossingPageBoundary( Byte OpcodeToTest, Byte CPU::*RegisterToTest );
+    void TestLoadRegisterAbsoluteYWhenCrossingPageBoundary( Byte OpcodeToTest, Byte CPU::*RegisterToTest );
+};
+
+static void VerifyUnmodifiedFlagsFromLoadRegister( const CPU& cpu, const CPU& CPUCopy) { 
+    EXPECT_EQ( cpu.Flag.C, CPUCopy.Flag.C);
+    EXPECT_EQ( cpu.Flag.I, CPUCopy.Flag.I);
+    EXPECT_EQ( cpu.Flag.D, CPUCopy.Flag.D);
+    EXPECT_EQ( cpu.Flag.B, CPUCopy.Flag.B);
+    EXPECT_EQ( cpu.Flag.V, CPUCopy.Flag.V);
+}
+
+void M6502AndEorOraTests::TestLoadRegisterImmediate( Byte OpcodeToTest, Byte CPU::*RegisterToTest )
+{
+    // Given: 
+    mem[0xFFFC] = OpcodeToTest;
+    mem[0xFFFD] = 0x84;
+
+    // When:
+    CPU CPUCopy = cpu;
+    s32 CyclesUsed = cpu.Execute( 2, mem );
+
+    // Then: 
+    EXPECT_EQ( cpu.*RegisterToTest, 0x84 );
+    EXPECT_EQ( CyclesUsed, 2 );
+    EXPECT_FALSE( cpu.Flag.Z );
+    EXPECT_TRUE( cpu.Flag.N );
+    VerifyUnmodifiedFlagsFromLoadRegister( cpu, CPUCopy );
+}
+
+TEST_F( M6502AndEorOraTests, LDAImmediateCanLoadAValueIntoTheARegister ) 
+{
+    TestLoadRegisterImmediate( CPU::INS_LDA_IM, &CPU::A );
+}
+
+TEST_F( M6502AndEorOraTests, LDAImmediateCanAffectTheZeroFlag ) 
+{
+    // Given: 
+    cpu.A = 0x44;
+    mem[0xFFFC] = CPU::INS_LDA_IM;
+    mem[0xFFFD] = 0x0;
+    CPU CPUCopy = cpu;
+
+    // When:   
+    cpu.Execute( 2, mem );
+
+    // Then: 
+    EXPECT_TRUE( cpu.Flag.Z );
+    EXPECT_FALSE( cpu.Flag.N );
+    VerifyUnmodifiedFlagsFromLoadRegister( cpu, CPUCopy );
+}
+
+void M6502AndEorOraTests::TestLoadRegisterZeroPage( Byte OpcodeToTest, Byte CPU::*RegisterToTest )
+{
+    // Given: 
+    mem[0xFFFC] = OpcodeToTest;
+    mem[0xFFFD] = 0x42;
+    mem[0x0042] = 0x37;
+
+    // When:
+    CPU CPUCopy = cpu;
+    s32 CyclesUsed = cpu.Execute( 3, mem );
+
+    // Then: 
+    EXPECT_EQ( cpu.*RegisterToTest, 0x37 );
+    EXPECT_EQ( CyclesUsed, 3 );
+    EXPECT_FALSE( cpu.Flag.Z );
+    EXPECT_FALSE( cpu.Flag.N );
+    VerifyUnmodifiedFlagsFromLoadRegister( cpu, CPUCopy );
+}
+
+TEST_F( M6502AndEorOraTests, LDAZeroPageCanLoadAValueIntoTheARegister ) 
+{
+    TestLoadRegisterZeroPage( CPU::INS_LDA_ZP, &CPU::A );
+}
+
+void M6502AndEorOraTests::TestLoadRegisterZeroPageX( Byte OpcodeToTest, Byte CPU::*RegisterToTest )
+{
+    // Given: 
+    cpu.X = 5;
+    mem[0xFFFC] = OpcodeToTest;
+    mem[0xFFFD] = 0x42;
+    mem[0x0047] = 0x37;
+
+    // When:
+    CPU CPUCopy = cpu;
+    s32 CyclesUsed = cpu.Execute( 4, mem );
+
+    // Then: 
+    EXPECT_EQ( cpu.*RegisterToTest, 0x37 );
+    EXPECT_EQ( CyclesUsed, 4 );
+    EXPECT_FALSE( cpu.Flag.Z );
+    EXPECT_FALSE( cpu.Flag.N );
+    VerifyUnmodifiedFlagsFromLoadRegister( cpu, CPUCopy );
+}
+
+void M6502AndEorOraTests::TestLoadRegisterZeroPageY( Byte OpcodeToTest, Byte CPU::*RegisterToTest )
+{
+    // Given: 
+    cpu.Y = 5;
+    mem[0xFFFC] = OpcodeToTest;
+    mem[0xFFFD] = 0x42;
+    mem[0x0047] = 0x37;
+
+    // When:
+    CPU CPUCopy = cpu;
+    s32 CyclesUsed = cpu.Execute( 4, mem );
+
+    // Then: 
+    EXPECT_EQ( cpu.*RegisterToTest, 0x37 );
+    EXPECT_EQ( CyclesUsed, 4 );
+    EXPECT_FALSE( cpu.Flag.Z );
+    EXPECT_FALSE( cpu.Flag.N );
+    VerifyUnmodifiedFlagsFromLoadRegister( cpu, CPUCopy );
+}
+
+TEST_F( M6502AndEorOraTests, LDAZeroPageXCanLoadAValueIntoTheARegister ) 
+{
+    TestLoadRegisterZeroPageX( CPU::INS_LDA_ZPX, &CPU::A );
+}
+
+TEST_F( M6502AndEorOraTests, LDAZeroPageXCanLoadAValueIntoTheARegisterWhenItWraps ) 
+{
+    // Given: 
+    cpu.X = 0xFF;
+    // Start - Inline a little program
+    mem[0xFFFC] = CPU::INS_LDA_ZPX;
+    mem[0xFFFD] = 0x80;
+    mem[0x007F] = 0x37;
+    // End - inline a little program
+
+    // When:
+    CPU CPUCopy = cpu;
+    s32 CyclesUsed = cpu.Execute( 4, mem );
+
+    // Then: 
+    EXPECT_EQ( cpu.A, 0x37 );
+    EXPECT_EQ( CyclesUsed, 4 );
+    EXPECT_FALSE( cpu.Flag.Z );
+    EXPECT_FALSE( cpu.Flag.N );
+    VerifyUnmodifiedFlagsFromLoadRegister( cpu, CPUCopy );
+}
+
+void M6502AndEorOraTests::TestLoadRegisterAbsolute( Byte OpcodeToTest, Byte CPU::*RegisterToTest )
+{
+    // Given: 
+    cpu.Flag.Z = cpu.Flag.N = true;
+    mem[0xFFFC] = OpcodeToTest;
+    mem[0xFFFD] = 0x80;
+    mem[0xFFFE] = 0x44; // 0x4480
+    mem[0x4480] = 0x37;
+    constexpr s32 EXPECTED_CYCLES = 4;
+    CPU CPUCopy = cpu;
+
+    // When:
+    s32 CyclesUsed = cpu.Execute( EXPECTED_CYCLES, mem );
+
+    // Then: 
+    EXPECT_EQ( cpu.*RegisterToTest, 0x37 );
+    EXPECT_EQ( CyclesUsed, EXPECTED_CYCLES );
+    EXPECT_FALSE( cpu.Flag.Z );
+    EXPECT_FALSE( cpu.Flag.N );
+    VerifyUnmodifiedFlagsFromLoadRegister( cpu, CPUCopy );
+}
+
+TEST_F( M6502AndEorOraTests, LDAAbsoluteCanLoadAValueIntoTheARegister ) 
+{
+    TestLoadRegisterAbsolute( CPU::INS_LDA_ABS, &CPU::A );
+}
+
+void M6502AndEorOraTests::TestLoadRegisterAbsoluteX( Byte OpcodeToTest, Byte CPU::*RegisterToTest )
+{
+    // Given: ;
+    cpu.Flag.Z = cpu.Flag.N = true;
+    cpu.X = 1;
+    mem[0xFFFC] = OpcodeToTest;
+    mem[0xFFFD] = 0x80;
+    mem[0xFFFE] = 0x44; // 0x4480
+    mem[0x4481] = 0x37;
+    constexpr s32 EXPECTED_CYCLES = 4;
+    CPU CPUCopy = cpu;
+
+    // When:
+    s32 CyclesUsed = cpu.Execute( EXPECTED_CYCLES, mem );
+
+    // Then: 
+    EXPECT_EQ( cpu.*RegisterToTest, 0x37 );
+    EXPECT_EQ( CyclesUsed, EXPECTED_CYCLES );
+    EXPECT_FALSE( cpu.Flag.Z );
+    EXPECT_FALSE( cpu.Flag.N );
+    VerifyUnmodifiedFlagsFromLoadRegister( cpu, CPUCopy );
+}
+
+void M6502AndEorOraTests::TestLoadRegisterAbsoluteY( Byte OpcodeToTest, Byte CPU::*RegisterToTest )
+{
+    // Given: ;
+    cpu.Flag.Z = cpu.Flag.N = true;
+    cpu.Y = 1;
+    mem[0xFFFC] = OpcodeToTest;
+    mem[0xFFFD] = 0x80;
+    mem[0xFFFE] = 0x44; // 0x4480
+    mem[0x4481] = 0x37;
+    constexpr s32 EXPECTED_CYCLES = 4;
+    CPU CPUCopy = cpu;
+
+    // When:
+    s32 CyclesUsed = cpu.Execute( EXPECTED_CYCLES, mem );
+
+    // Then: 
+    EXPECT_EQ( cpu.*RegisterToTest, 0x37 );
+    EXPECT_EQ( CyclesUsed, EXPECTED_CYCLES );
+    EXPECT_FALSE( cpu.Flag.Z );
+    EXPECT_FALSE( cpu.Flag.N );
+    VerifyUnmodifiedFlagsFromLoadRegister( cpu, CPUCopy );
+}
+
+TEST_F( M6502AndEorOraTests, LDAAbsoluteXCanLoadAValueIntoTheARegister ) 
+{
+    TestLoadRegisterAbsoluteX( CPU::INS_LDA_ABSX, &CPU::A );
+}
+
+void M6502AndEorOraTests::TestLoadRegisterAbsoluteXWhenCrossingPageBoundary( Byte OpcodeToTest, Byte CPU::*RegisterToTest )
+{
+    // Given: ;
+    cpu.X = 0xFF;
+    mem[0xFFFC] = OpcodeToTest;
+    mem[0xFFFD] = 0x02;
+    mem[0xFFFE] = 0x44; // 0x4480
+    mem[0x4501] = 0x37; // 0x4402 + 0xFF crosses page boundary!
+    constexpr s32 EXPECTED_CYCLES = 5;
+    CPU CPUCopy = cpu;
+
+    // When:
+    s32 CyclesUsed = cpu.Execute( EXPECTED_CYCLES, mem );
+
+    // Then: 
+    EXPECT_EQ( cpu.*RegisterToTest, 0x37 );
+    EXPECT_EQ( CyclesUsed, EXPECTED_CYCLES );
+    EXPECT_FALSE( cpu.Flag.Z );
+    EXPECT_FALSE( cpu.Flag.N );
+    VerifyUnmodifiedFlagsFromLoadRegister( cpu, CPUCopy );
+}
+
+void M6502AndEorOraTests::TestLoadRegisterAbsoluteYWhenCrossingPageBoundary( Byte OpcodeToTest, Byte CPU::*RegisterToTest )
+{
+    // Given: ;
+    cpu.Y = 0xFF;
+    mem[0xFFFC] = OpcodeToTest;
+    mem[0xFFFD] = 0x02;
+    mem[0xFFFE] = 0x44; // 0x4480
+    mem[0x4501] = 0x37; // 0x4402 + 0xFF crosses page boundary!
+    constexpr s32 EXPECTED_CYCLES = 5;
+    CPU CPUCopy = cpu;
+
+    // When:
+    s32 CyclesUsed = cpu.Execute( EXPECTED_CYCLES, mem );
+
+    // Then: 
+    EXPECT_EQ( cpu.*RegisterToTest, 0x37 );
+    EXPECT_EQ( CyclesUsed, EXPECTED_CYCLES );
+    EXPECT_FALSE( cpu.Flag.Z );
+    EXPECT_FALSE( cpu.Flag.N );
+    VerifyUnmodifiedFlagsFromLoadRegister( cpu, CPUCopy );
+}
+
+TEST_F( M6502AndEorOraTests, LDAAbsoluteXCanLoadAValueIntoTheARegisterWhenItCrossesAPageBoundary ) 
+{
+    TestLoadRegisterAbsoluteXWhenCrossingPageBoundary( CPU::INS_LDA_ABSX, &CPU::A );
+}
+
+TEST_F( M6502AndEorOraTests, LDAAbsoluteYCanLoadAValueIntoTheARegister ) 
+{
+    TestLoadRegisterAbsoluteY( CPU::INS_LDA_ABSY, &CPU::A );
+}
+
+TEST_F( M6502AndEorOraTests, LDAAbsoluteYCanLoadAValueIntoTheARegisterWhenItCrossesAPageBoundary ) 
+{
+    TestLoadRegisterAbsoluteYWhenCrossingPageBoundary( CPU::INS_LDA_ABSY, &CPU::A );
+}
+
+TEST_F( M6502AndEorOraTests, LDAIndirectXCanLoadAValueIntoTheARegister ) 
+{
+    // Given: ;
+    cpu.Flag.Z = cpu.Flag.N = true;
+    cpu.X = 0x04;
+    mem[0xFFFC] = CPU::INS_LDA_INDX;
+    mem[0xFFFD] = 0x02;
+    mem[0x0006] = 0x00; // 0x2 + 0x4
+    mem[0x0007] = 0x80; 
+    mem[0x8000] = 0x37;
+    constexpr s32 EXPECTED_CYCLES = 6;
+    CPU CPUCopy = cpu;
+
+    // When:
+    s32 CyclesUsed = cpu.Execute( EXPECTED_CYCLES, mem );
+
+    // Then: 
+    EXPECT_EQ( cpu.A, 0x37 );
+    EXPECT_EQ( CyclesUsed, EXPECTED_CYCLES );
+    EXPECT_FALSE( cpu.Flag.Z );
+    EXPECT_FALSE( cpu.Flag.N );
+    VerifyUnmodifiedFlagsFromLoadRegister( cpu, CPUCopy );
+}
+
+TEST_F( M6502AndEorOraTests, LDAIndirectYCanLoadAValueIntoTheARegister ) 
+{
+    // Given: ;
+    cpu.Flag.Z = cpu.Flag.N = true;
+    cpu.Y = 0x04;
+    mem[0xFFFC] = CPU::INS_LDA_INDY;
+    mem[0xFFFD] = 0x02;
+    mem[0x0002] = 0x00; 
+    mem[0x0003] = 0x80; 
+    mem[0x8004] = 0x37; // 0x8000 + 0x4
+    constexpr s32 EXPECTED_CYCLES = 5;
+    CPU CPUCopy = cpu;
+
+    // When:
+    s32 CyclesUsed = cpu.Execute( EXPECTED_CYCLES, mem );
+
+    // Then: 
+    EXPECT_EQ( cpu.A, 0x37 );
+    EXPECT_EQ( CyclesUsed, EXPECTED_CYCLES );
+    EXPECT_FALSE( cpu.Flag.Z );
+    EXPECT_FALSE( cpu.Flag.N );
+    VerifyUnmodifiedFlagsFromLoadRegister( cpu, CPUCopy );
+}
+
+TEST_F( M6502AndEorOraTests, LDAIndirectYCanLoadAValueIntoTheARegisterWhenItCrossesAPageBoundary ) 
+{
+    // Given: ;
+    cpu.Y = 0xFF;
+    mem[0xFFFC] = CPU::INS_LDA_INDY;
+    mem[0xFFFD] = 0x02;
+    mem[0x0002] = 0x02; 
+    mem[0x0003] = 0x80; 
+    mem[0x8101] = 0x37; // 0x8002 + 0xFF
+    constexpr s32 EXPECTED_CYCLES = 6;
+    CPU CPUCopy = cpu;
+
+    // When:
+    s32 CyclesUsed = cpu.Execute( EXPECTED_CYCLES, mem );
+
+    // Then: 
+    EXPECT_EQ( cpu.A, 0x37 );
+    EXPECT_EQ( CyclesUsed, EXPECTED_CYCLES );
+    EXPECT_FALSE( cpu.Flag.Z );
+    EXPECT_FALSE( cpu.Flag.N );
+    VerifyUnmodifiedFlagsFromLoadRegister( cpu, CPUCopy );
+}
