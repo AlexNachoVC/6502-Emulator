c8d763e15b6c50409b7160fdf72ba2e22f9b07bd
Alejandro <alexnachovc12@gmail.com>
2024-07-22 19:07:08 -0600

    BNE Implemented

--- 6502Lib/src/private/m6502.cpp
+++ 6502Lib/src/private/m6502.cpp
@@ -532,35 +532,11 @@ m6502::s32 m6502::CPU::Execute(s32 Cycles, Mem &memory)
             } break;
             case INS_BEQ: 
             {
-                Byte Offset = FetchByte( Cycles, memory );
-                if ( Flag.Z ) 
-                {
-                    const Word PCOld = PC;
-                    PC += static_cast<SByte>( Offset );
-                    Cycles--;
-
-                    const bool PageChanged = ( PC >> 8) != (PCOld >> 8);
-                    if ( PageChanged )
-                    {
-                        Cycles -= 2;
-                    }
-                }
+                BranchIf( Flag.Z, true );
             } break;
             case INS_BNE: 
             {
-                Byte Offset = FetchByte( Cycles, memory );
-                if ( !Flag.Z ) 
-                {
-                    const Word PCOld = PC;
-                    PC += static_cast<SByte>( Offset );
-                    Cycles--;
-
-                    const bool PageChanged = ( PC >> 8) != (PCOld >> 8);
-                    if ( PageChanged )
-                    {
-                        Cycles -= 2;
-                    }
-                }
+                BranchIf( Flag.Z, false );
             } break;
             default:
             {
