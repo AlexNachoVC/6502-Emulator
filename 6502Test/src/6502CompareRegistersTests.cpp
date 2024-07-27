531b8d8ffcd804fff53814aa12e64ece413ef61d
Alejandro <alexnachovc12@gmail.com>
2024-07-26 22:56:31 -0600

    CMP Immediate, negative value tests

--- 6502Test/src/6502CompareRegistersTests.cpp
+++ 6502Test/src/6502CompareRegistersTests.cpp
@@ -78,3 +78,30 @@ TEST_F( M6502CompareRegistersTests, CMPImmediateCanCompareTwoDifferentPositiveVa
         EXPECT_TRUE( cpu.Flag.C );
         ExpectUnaffectedRegisters( CPUCopy );
 }
+
+TEST_F( M6502CompareRegistersTests, CMPImmediateCanCompareANegativeNumberToAPositive )
+{
+    // given:
+        using namespace m6502;
+        cpu.Reset( 0xFF00, mem );
+        cpu.Flag.Z = false;
+        cpu.Flag.N = true;
+        cpu.Flag.C = false;
+        cpu.A = 130;    // Negative Number!
+        mem[0xFF00] = CPU::INS_CMP_IM;
+        mem[0xFF01] = 26;
+        constexpr s32 EXPECTED_CYCLES = 2;
+        CPU CPUCopy = cpu;
+
+        // when:
+        const s32 ActualCycles = cpu.Execute( EXPECTED_CYCLES, mem );
+
+        // then:
+        EXPECT_EQ( ActualCycles, EXPECTED_CYCLES );
+        EXPECT_EQ( cpu.A, 26 );
+        EXPECT_TRUE( cpu.Flag.Z );
+        EXPECT_FALSE( cpu.Flag.N );
+        EXPECT_TRUE( cpu.Flag.C );
+        ExpectUnaffectedRegisters( CPUCopy );
+}
+
