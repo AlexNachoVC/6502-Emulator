c801303148f5b4bea0a4e567b6e1ffdc60fc56ce
Alejandro <alexnachovc12@gmail.com>
2024-07-30 15:34:26 -0600

    ASL Zero Page X, tests and implementation

--- 6502Lib/src/private/m6502.cpp
+++ 6502Lib/src/private/m6502.cpp
@@ -788,6 +788,13 @@ m6502::s32 m6502::CPU::Execute(s32 Cycles, Mem &memory)
                 Byte Result = ASL( Operand );
                 WriteByte( Result, Cycles, Address, memory );
             } break;
+            case INS_ASL_ZPX:
+            {
+                Word Address = AddressZeroPageX( Cycles, memory );
+                Byte Operand = ReadByte( Cycles, Address, memory );
+                Byte Result = ASL( Operand );
+                WriteByte( Result, Cycles, Address, memory );
+            } break;
             default:
             {
                 printf("Instruction %d not handled\n", Ins);
--- 6502Test/src/6502ShiftsTests.cpp
+++ 6502Test/src/6502ShiftsTests.cpp
@@ -113,3 +113,55 @@ TEST_F( M6502ShiftsTests, ASLZeroPageCanShiftANegativeValue )
     EXPECT_FALSE( cpu.Flag.Z );
     EXPECT_TRUE( cpu.Flag.N );
 }
+
+TEST_F( M6502ShiftsTests, ASLZeroPageXCanShiftTheValueOfOne )
+{
+	// given:
+	using namespace m6502;
+	cpu.Reset( 0xFF00, mem );
+	cpu.Flag.C = true;
+	cpu.Flag.Z = true;
+	cpu.Flag.N = true;
+	cpu.X = 0x10;
+	mem[0xFF00] = CPU::INS_ASL_ZPX;
+	mem[0xFF01] = 0x42;
+	mem[0x0042 + 0x10] = 1;
+	constexpr s32 EXPECTED_CYCLES = 6;
+	CPU CPUCopy = cpu;
+
+	// when:
+	const s32 ActualCycles = cpu.Execute( EXPECTED_CYCLES, mem );
+
+	// then:
+	EXPECT_EQ( ActualCycles, EXPECTED_CYCLES );
+	EXPECT_EQ( mem[0x0042 + 0x10], 2 );
+	EXPECT_FALSE( cpu.Flag.C );
+	EXPECT_FALSE( cpu.Flag.Z );
+	EXPECT_FALSE( cpu.Flag.N );
+}
+
+TEST_F( M6502ShiftsTests, ASLZeroPageXCanShiftANegativeValue )
+{
+	// given:
+	using namespace m6502;
+	cpu.Reset( 0xFF00, mem );
+	cpu.Flag.C = false;
+	cpu.Flag.Z = true;
+	cpu.Flag.N = false;
+	cpu.X = 0x10;
+	mem[0xFF00] = CPU::INS_ASL_ZPX;
+	mem[0xFF01] = 0x42;
+	mem[0x0042 + 0x10] = 0b11000010;
+	constexpr s32 EXPECTED_CYCLES = 6;
+	CPU CPUCopy = cpu;
+
+	// when:
+	const s32 ActualCycles = cpu.Execute( EXPECTED_CYCLES, mem );
+
+	// then:
+	EXPECT_EQ( ActualCycles, EXPECTED_CYCLES );
+	EXPECT_EQ( mem[0x0042 + 0x10], 0b10000100 );
+	EXPECT_TRUE( cpu.Flag.C );
+	EXPECT_FALSE( cpu.Flag.Z );
+	EXPECT_TRUE( cpu.Flag.N );
+}
