580c3803c3cb06876a4906fcc8aaa7e0c61ace76
Alejandro <alexnachovc12@gmail.com>
2024-07-30 15:13:43 -0600

    Another ASL Accumulator Test

--- 6502Test/src/6502ShiftsTests.cpp
+++ 6502Test/src/6502ShiftsTests.cpp
@@ -25,8 +25,8 @@ TEST_F( M6502ShiftsTests, ASLCanShiftTheValueOfOne )
 {
     // Given:
     cpu.Reset( 0xFF00, mem );
-    cpu.Flag.Z = true;
     cpu.Flag.C = true;
+    cpu.Flag.Z = true;
     cpu.Flag.N = true;
     cpu.A = 1;
     mem[0xFF00] = CPU::INS_ASL;
@@ -42,4 +42,27 @@ TEST_F( M6502ShiftsTests, ASLCanShiftTheValueOfOne )
     EXPECT_FALSE( cpu.Flag.C );
     EXPECT_FALSE( cpu.Flag.Z );
     EXPECT_FALSE( cpu.Flag.N );
+}
+
+TEST_F( M6502ShiftsTests, ASLCanShiftANegativeValue )
+{
+    // Given:
+    cpu.Reset( 0xFF00, mem );
+    cpu.Flag.C = false;
+    cpu.Flag.Z = true;
+    cpu.Flag.N = false;
+    cpu.A = 0b11000010;
+    mem[0xFF00] = CPU::INS_ASL;
+    constexpr s32 EXPECTED_CYCLES = 2;  
+    CPU CPUCopy = cpu;
+
+    // When:
+    const s32 ActualCycles = cpu.Execute( EXPECTED_CYCLES, mem );
+
+    // Then:
+    EXPECT_EQ( ActualCycles, EXPECTED_CYCLES );
+    EXPECT_EQ( cpu.A, 0b10000100 );
+    EXPECT_TRUE( cpu.Flag.C );
+    EXPECT_FALSE( cpu.Flag.Z );
+    EXPECT_TRUE( cpu.Flag.N );
 }
