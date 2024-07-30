2a3c8ededb5c89a00856931398c8162dd4645d0f
Alejandro <alexnachovc12@gmail.com>
2024-07-30 15:18:45 -0600

    ASL Zero Page tests

--- 6502Test/src/6502ShiftsTests.cpp
+++ 6502Test/src/6502ShiftsTests.cpp
@@ -19,8 +19,6 @@ protected:
 
 };
 
-
-
 TEST_F( M6502ShiftsTests, ASLCanShiftTheValueOfOne )
 {
     // Given:
@@ -62,6 +60,55 @@ TEST_F( M6502ShiftsTests, ASLCanShiftANegativeValue )
     // Then:
     EXPECT_EQ( ActualCycles, EXPECTED_CYCLES );
     EXPECT_EQ( cpu.A, 0b10000100 );
+    EXPECT_TRUE( cpu.Flag.C );
+    EXPECT_FALSE( cpu.Flag.Z );
+    EXPECT_TRUE( cpu.Flag.N );
+}
+
+TEST_F( M6502ShiftsTests, ASLZeroPageCanShiftTheValueOfOne )
+{
+    // Given:
+    cpu.Reset( 0xFF00, mem );
+    cpu.Flag.C = true;
+    cpu.Flag.Z = true;
+    cpu.Flag.N = true;
+    cpu.A = 1;
+    mem[0xFF00] = CPU::INS_ASL_ZP;
+    mem[0xFF01] = 0x42;
+    mem[0x0042] = 1;
+    constexpr s32 EXPECTED_CYCLES = 5;  
+    CPU CPUCopy = cpu;
+
+    // When:
+    const s32 ActualCycles = cpu.Execute( EXPECTED_CYCLES, mem );
+
+    // Then:
+    EXPECT_EQ( ActualCycles, EXPECTED_CYCLES );
+    EXPECT_EQ( mem[0x0042], 2 );
+    EXPECT_FALSE( cpu.Flag.C );
+    EXPECT_FALSE( cpu.Flag.Z );
+    EXPECT_FALSE( cpu.Flag.N );
+}
+
+TEST_F( M6502ShiftsTests, ASLZeroPageCanShiftANegativeValue )
+{
+    // Given:
+    cpu.Reset( 0xFF00, mem );
+    cpu.Flag.C = false;
+    cpu.Flag.Z = true;
+    cpu.Flag.N = false;
+    mem[0xFF00] = CPU::INS_ASL_ZP;
+    mem[0xFF01] = 0x42;
+    mem[0x0042] = 0b11000010;
+    constexpr s32 EXPECTED_CYCLES = 5;  
+    CPU CPUCopy = cpu;
+
+    // When:
+    const s32 ActualCycles = cpu.Execute( EXPECTED_CYCLES, mem );
+
+    // Then:
+    EXPECT_EQ( ActualCycles, EXPECTED_CYCLES );
+    EXPECT_EQ( mem[0x0042], 0b10000100 );
     EXPECT_TRUE( cpu.Flag.C );
     EXPECT_FALSE( cpu.Flag.Z );
     EXPECT_TRUE( cpu.Flag.N );
