d84813c4ae6fc25fb34186b31f557617dae55ee1
Alejandro <alexnachovc12@gmail.com>
2024-07-30 19:50:47 -0600

    LSR Zero Page Test

--- 6502Test/src/6502ShiftsTests.cpp
+++ 6502Test/src/6502ShiftsTests.cpp
@@ -317,3 +317,53 @@ TEST_F( M6502ShiftsTests, LSRCanShiftAZeroIntoTheCarryFlag )
     EXPECT_FALSE( cpu.Flag.Z );
     EXPECT_FALSE( cpu.Flag.N );
 }
+
+TEST_F( M6502ShiftsTests, LSRZeroPageCanShiftTheValueOfOne )
+{
+    // Given:
+    cpu.Reset( 0xFF00, mem );
+    cpu.Flag.C = false;
+    cpu.Flag.Z = false;
+    cpu.Flag.N = true;
+    cpu.A = 1;
+    mem[0xFF00] = CPU::INS_LSR_ZP;
+    mem[0xFF01] = 0x42;
+    mem[0x0042] = 1;
+    constexpr s32 EXPECTED_CYCLES = 5;  
+    CPU CPUCopy = cpu;
+
+    // When:
+    const s32 ActualCycles = cpu.Execute( EXPECTED_CYCLES, mem );
+
+    // Then:
+    EXPECT_EQ( ActualCycles, EXPECTED_CYCLES );
+    EXPECT_EQ( mem[0x0042], 0 );
+    EXPECT_TRUE( cpu.Flag.C );
+    EXPECT_TRUE( cpu.Flag.Z );
+    EXPECT_FALSE( cpu.Flag.N );
+}
+
+TEST_F( M6502ShiftsTests, LSRZeroPageCanShiftAZeroIntoTheCarryFlag )
+{
+    // Given:
+    cpu.Reset( 0xFF00, mem );
+    cpu.Flag.C = true;
+    cpu.Flag.Z = false;
+    cpu.Flag.N = true;
+    cpu.A = 8;
+    mem[0xFF00] = CPU::INS_LSR_ZP;
+    mem[0xFF01] = 0x42;
+    mem[0x0042] = 8;
+    constexpr s32 EXPECTED_CYCLES = 5;  
+    CPU CPUCopy = cpu;
+
+    // When:
+    const s32 ActualCycles = cpu.Execute( EXPECTED_CYCLES, mem );
+
+    // Then:
+    EXPECT_EQ( ActualCycles, EXPECTED_CYCLES );
+    EXPECT_EQ( mem[0x0042], 4 );
+    EXPECT_FALSE( cpu.Flag.C );
+    EXPECT_FALSE( cpu.Flag.Z );
+    EXPECT_FALSE( cpu.Flag.N );
+}
