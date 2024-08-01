99c8db638f6cca76aa06d559f8eebda185d2c493
Alejandro <alexnachovc12@gmail.com>
2024-07-31 23:39:52 -0600

    ROL Accummulator, Carry out test

--- 6502Test/src/6502ShiftsTests.cpp
+++ 6502Test/src/6502ShiftsTests.cpp
@@ -549,3 +549,27 @@ TEST_F( M6502ShiftsTests, ROLCanShiftABitOutOfTheCarryFlag )
 	EXPECT_FALSE( cpu.Flag.Z );
 	EXPECT_FALSE( cpu.Flag.N );
 }
+
+TEST_F( M6502ShiftsTests, ROLCanShiftABitIntoTheCarryFlag )
+{
+	// given:
+	using namespace m6502;
+	cpu.Reset( 0xFF00, mem );
+	cpu.Flag.C = false;
+	cpu.Flag.Z = false;
+	cpu.Flag.N = true;
+	cpu.A = 0b10000000;
+	mem[0xFF00] = CPU::INS_ROL;
+	constexpr s32 EXPECTED_CYCLES = 2;
+	CPU CPUCopy = cpu;
+
+	// when:
+	const s32 ActualCycles = cpu.Execute( EXPECTED_CYCLES, mem );
+
+	// then:
+	EXPECT_EQ( ActualCycles, EXPECTED_CYCLES );
+	EXPECT_EQ( cpu.A,  0 );
+	EXPECT_TRUE( cpu.Flag.C );
+	EXPECT_TRUE( cpu.Flag.Z );
+	EXPECT_FALSE( cpu.Flag.N );
+}
