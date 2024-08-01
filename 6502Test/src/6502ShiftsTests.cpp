bc5868c085bfd7dd45c6945ca19847f9061bc1a4
Alejandro <alexnachovc12@gmail.com>
2024-07-31 23:44:41 -0600

    ROL Accumulator, negative result test

--- 6502Test/src/6502ShiftsTests.cpp
+++ 6502Test/src/6502ShiftsTests.cpp
@@ -597,3 +597,27 @@ TEST_F( M6502ShiftsTests, ROLCanShiftZeroWithNoCarry )
 	EXPECT_TRUE( cpu.Flag.Z );
 	EXPECT_FALSE( cpu.Flag.N );
 }
+
+TEST_F( M6502ShiftsTests, ROLCanShiftAValueThatResultInANegativeValue )
+{
+	// given:
+	using namespace m6502;
+	cpu.Reset( 0xFF00, mem );
+	cpu.Flag.C = true;
+	cpu.Flag.Z = false;
+	cpu.Flag.N = false;
+	cpu.A = 0b01110011;
+	mem[0xFF00] = CPU::INS_ROL;
+	constexpr s32 EXPECTED_CYCLES = 2;
+	CPU CPUCopy = cpu;
+
+	// when:
+	const s32 ActualCycles = cpu.Execute( EXPECTED_CYCLES, mem );
+
+	// then:
+	EXPECT_EQ( ActualCycles, EXPECTED_CYCLES );
+	EXPECT_EQ( cpu.A,  0b11100111 );
+	EXPECT_FALSE( cpu.Flag.C );
+	EXPECT_FALSE( cpu.Flag.Z );
+	EXPECT_TRUE( cpu.Flag.N );
+}
